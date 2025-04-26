7041ed40b2f886ebe801b898c301bf71cef800a0
Johanna Bayer <hannabayer.89@gmail.com>
2025-04-26 13:47:45 +0200

    add slash

--- day3/reproducible-research-project/tests/conftest.py
+++ day3/reproducible-research-project/tests/conftest.py
@@ -2,5 +2,5 @@ import pytest
 import pandas as pd
 @pytest.fixture(scope="session")
 def data():
-    df = pd.read_csv("content/RSE_Juelich/day3/reproducible-research-project/data/clean/clean.csv")
+    df = pd.read_csv("/content/RSE_Juelich/day3/reproducible-research-project/data/clean/clean.csv")
     return df
