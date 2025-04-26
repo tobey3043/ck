e8c347892b53c9c515e2351a837b36d1c2010db5
Johanna Bayer <hannabayer.89@gmail.com>
2025-04-26 13:50:11 +0200

    add correct number of rows

--- day3/reproducible-research-project/tests/data_test.py
+++ day3/reproducible-research-project/tests/data_test.py
@@ -3,4 +3,4 @@ def test_clean_data(data):
     assert data.isna().sum().sum() == 0, "Data contains missing values"
     
 def test_number_of_row(data):
-    assert len(data) == 1000, "Data does not have the expected number of rows"
+    assert len(data) == 909, "Data does not have the expected number of rows"
