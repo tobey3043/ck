f5c87cf7e8db480ed8cc17217453651d34239235
Johanna Bayer <hannabayer.89@gmail.com>
2025-04-30 20:03:24 +0200

    move Raedme and remove solutions day 3

--- day3/reproducible-research-project/tests/conftest.py
+++
@@ -1,6 +0,0 @@
-import pytest
-import pandas as pd
-@pytest.fixture(scope="session")
-def data():
-    df = pd.read_csv("/content/RSE_Juelich/day3/reproducible-research-project/data/clean/clean.csv")
-    return df
--- day3/reproducible-research-project/tests/data_test.py
+++
@@ -1,6 +0,0 @@
-import pytest
-def test_clean_data(data):
-    assert data.isna().sum().sum() == 0, "Data contains missing values"
-    
-def test_number_of_row(data):
-    assert len(data) == 909, "Data does not have the expected number of rows"
